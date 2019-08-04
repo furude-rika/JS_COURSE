6ec4fccafb86c5fc54aaac38d3c3e97bc3d6622a
Anastasia Lukina <gr_14a_34@ukr.net>
2019-08-04 20:41:56 +0300

    commit

---
+++ HW11/part3/script.js
@@ -0,0 +1,20 @@
+var messages = ["backspace", "enter", "shift", "control", "delete", "space", "subtract"]
+
+messages.getKey = () => new Date().toLocaleString().split(", ")[1]
+var log = {}
+
+var sendMessage = message => new Promise(
+    resolve => setTimeout(() => resolve(message), Math.random() * 7000)
+)
+
+var sendAll = () => {
+    var index = 0
+    function recursive () {
+       sendMessage(messages[index++])
+            .then(item => log[messages.getKey()] = item)
+            index <= messages.length ? recursive() : null
+        }
+    recursive ()
+}
+
+sendAll()
