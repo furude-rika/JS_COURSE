969c9334d816234dc49586e3dc0ecb66926518fb
Anastasia Lukina <gr_14a_34@ukr.net>
2019-08-04 15:25:01 +0300

    commit

--- HW11/part1/script.js
+++ HW11/part1/script.js
@@ -4,9 +4,9 @@ response.onreadystatechange = function(event) {
    if (this.readyState === 4 && this.status === 200) {
        let resp = JSON.parse(this.responseText)
         resp.forEach(elem => {
-            for (var x in resp) {
-                document.body.appendChild(document.createElement('img')).src = elem.ref
-                document.body.appendChild(document.createElement('h2')).innerHTML = elem.title
+            for (var x of elem) {
+                document.body.appendChild(document.createElement('img')).src = x.ref
+                document.body.appendChild(document.createElement('h2')).innerHTML = x.title
             }
         }) 
     }
