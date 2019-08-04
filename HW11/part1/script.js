d65eafd0fe9455bfe840f71ebc9303dc90a35115
Anastasia Lukina <gr_14a_34@ukr.net>
2019-08-04 16:40:30 +0300

    commit

--- HW11/part1/script.js
+++ HW11/part1/script.js
@@ -2,11 +2,11 @@ let response = new XMLHttpRequest()
 response.open('GET','data.json')
 response.onreadystatechange = function(event) {
    if (this.readyState === 4 && this.status === 200) {
-       let resp = JSON.stringify(JSON.parse(this.responseText))
+       let resp = JSON.parse(this.responseText)
         resp.forEach(elem => {
 	            document.body.appendChild(document.createElement('img')).src = elem.ref
 	            document.body.appendChild(document.createElement('h2')).innerHTML = elem.title
-    	}
+    	})
     }
 }
 response.send()
